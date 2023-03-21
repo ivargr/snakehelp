70d2277b54a805402ade1556796936453876c11b
Ivar Grytten <ivar.grytten@gmail.com>
2023-03-21 13:12:11 +0100

    min python

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ test_requirements = ['pytest>=3', "hypothesis"]
 setup(
     author="Ivar Grytten",
     author_email='ivar.grytten@gmail.com',
-    python_requires='>=3.6',
+    python_requires='>=3.8',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
