c970efa37b24287d098188a74d7b6762d573a2c6
Ivar Grytten <ivar.grytten@gmail.com>
2023-03-25 13:44:37 +0100

    fix

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_requirements = ['pytest>=3', "hypothesis", "pandas", "numpy", "shared_memor
 setup(
     author="Ivar Grytten",
     author_email='ivar.grytten@gmail.com',
-    python_requires='>=3.8',
+    python_requires='>=3.10',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
