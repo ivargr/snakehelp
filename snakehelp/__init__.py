69b4b127c86306d90b2b70237b9d74027e270e1b
BumpVersion Action <bumpversion@github-actions>
2023-03-25 14:20:55 +0000

    Bump version: 0.0.4 → 0.0.5

--- snakehelp/__init__.py
+++ snakehelp/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Ivar Grytten"""
 __email__ = 'ivar.grytten@gmail.com'
-__version__ = '0.0.4'
+__version__ = '0.0.5'
 
 from .parameters import parameters, result
 from .config import set_data_folder, get_data_folder
